a81a200f1a1a8a58d0d11dbe7578ea6b38d9f70d
Lucas Soares <soareslukas9090@gmail.com>
2023-04-10 17:05:08 -0300

    Commit com a contagem dos pontos e reset do game

--- app/src/main/java/com/estudos/geoquiz/MainActivity.kt
+++ app/src/main/java/com/estudos/geoquiz/MainActivity.kt
@@ -4,11 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import com.estudos.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import kotlin.random.Random
-
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
 
@@ -22,11 +21,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         Questions(R.string.question_americas, true),
         Questions(R.string.question_asia, true))
     private var currentIndex = 0
+    private var currentUsedIndex = 0
 
     private var hits = 0
-
-    private var prev = mutableListOf(currentIndex)
-    private var prevIndex = 0
 
     private var used = mutableListOf(currentIndex)
 
@@ -37,10 +34,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        buildQuestionList()
+
         binding.buttonTrue.setOnClickListener(this)
         binding.buttonFalse.setOnClickListener(this)
         binding.buttonNext.setOnClickListener(this)
         binding.buttonPrev.setOnClickListener(this)
+        binding.buttonReset.setOnClickListener(this)
 
     }
 
@@ -52,12 +52,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 answer = false
 
             if (questionBank[currentIndex].answer == answer) {
-                Snackbar.make(v, R.string.msgCorrect, Snackbar.LENGTH_SHORT).show()
                 hits++
+                Log.d(TAG, "Hits atualizado para ${hits}")
+                Snackbar.make(v, R.string.msgCorrect, Snackbar.LENGTH_SHORT).show()
                 update()
             }
+
             else{
                 Snackbar.make(v, R.string.msgWrong, Snackbar.LENGTH_LONG).show()
+                Log.d(TAG, "Errou")
                 update()
             }
 
@@ -67,27 +70,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             update()
         }
 
+        if (v.id == R.id.button_Prev){
+            previous(v)
+        }
+
         if (v.id == R.id.text_question){
             update()
         }
 
-        if (v.id == R.id.button_Prev){
-            previous(v)
+        if (v.id == R.id.button_Reset){
+            resetGame(v)
         }
 
     }
 
     private fun update(){
-        if(used.size != questionBank.size) {
-            var numTemp = Random.nextInt(0, (questionBank.size-1))
-            while (used.contains(numTemp)) {
-                numTemp = Random.nextInt(0, (questionBank.size-1))
-            }
-            used.add(numTemp)
-            currentIndex = numTemp
+        if(currentUsedIndex < (questionBank.size - 1)) {
+            currentUsedIndex++
+            Log.d(TAG, "${currentIndex} -- ${currentUsedIndex}")
+            currentIndex = used[currentUsedIndex]
             binding.textQuestion.setText(questionBank[currentIndex].textResId)
-            prevIndex+=1
-            prev.add(prevIndex, currentIndex)
         }
         else {
             finishGame()
@@ -95,21 +97,46 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun previous(v: View){
-        if (prevIndex > 0){
-            prevIndex-=1
-            currentIndex = prev[prevIndex]
+        if (currentUsedIndex > 0){
+            currentUsedIndex--
+            currentIndex = used[currentUsedIndex]
             binding.textQuestion.setText(questionBank[currentIndex].textResId)
-            used.removeLast()
         } else {
-            Snackbar.make(v, R.string.notPrev, Snackbar.LENGTH_LONG).show()
+            Snackbar.make(v, R.string.msgNotPrev, Snackbar.LENGTH_LONG).show()
         }
     }
 
     private fun finishGame(){
-        currentIndex = 0
-        prevIndex = 0
-        prev.clear()
+        binding.textQuestion.text = "Parabens, voce acertou ${hits} de ${questionBank.size}!"
+        binding.buttonPrev.isEnabled = false
+        binding.buttonNext.isEnabled = false
+        binding.buttonTrue.isEnabled = false
+        binding.buttonFalse.isEnabled = false
+    }
+
+    private fun buildQuestionList(){
         used.clear()
-        Toast.makeText(this,"Parabens, voce acertou ${hits} de ${questionBank.size}!",Toast.LENGTH_LONG)
+        while (used.size < 6) {
+            val numTemp = Random.nextInt(0, (questionBank.size))
+            if (!used.contains(numTemp)) {
+                used.add(numTemp)
+            }
+        }
+        Log.d(TAG, "${used}")
+        currentIndex = used[currentUsedIndex]
+        binding.textQuestion.setText(questionBank[currentIndex].textResId)
+    }
+
+    private fun resetGame(v: View){
+        currentIndex = 0
+        currentUsedIndex = 0
+        used.clear()
+        hits = 0
+        buildQuestionList()
+        Snackbar.make(v, R.string.msgReset, Snackbar.LENGTH_SHORT).show()
+        binding.buttonPrev.isEnabled = true
+        binding.buttonNext.isEnabled = true
+        binding.buttonTrue.isEnabled = true
+        binding.buttonFalse.isEnabled = true
     }
 }
